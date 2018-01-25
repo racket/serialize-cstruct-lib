4d0eb5c06ae972affc6df6905eb5584a9cf6315e
Matthew Flatt <mflatt@racket-lang.org>
2018-01-24 20:01:54 -0700

    Revert "fix problems with `_gcable` fields"
    This reverts commit c9264f176c3aca815915b599ddfa539541e095a6.

--- ffi/serialize-cstruct.rkt
+++ ffi/serialize-cstruct.rkt
@@ -114,13 +114,13 @@
               (lambda (s)
                 (force all-serializable)
                 (hash-set! cpointer-mapping s s)
+                (define inplace-bs (make-sized-byte-string s (ctype-sizeof _ID)))
                 (define bs
                   (if serialize-inplace
-                      (make-sized-byte-string s (ctype-sizeof _ID))
-                      (let ([mem (make-bytes (ctype-sizeof _ID))])
-                        (when (copy-any-non-pointers? _ID)
-                          (memcpy mem s 1 _ID))
-                        mem)))
+                      inplace-bs
+                      (let ([mem (malloc _ID 'atomic)])
+                        (memcpy mem inplace-bs 1 _ID)
+                        (make-sized-byte-string mem (ctype-sizeof _ID)))))
                 (vector bs (serialize-cstruct-pointers s)))
               (quote-syntax deser-ID)
               #t
@@ -184,6 +184,7 @@
       (array-base-type (array-type ((ctype-c->scheme ct) #f)))
       ct))
 
+
 (define (ctype-layout-base-type v)
   (if (vector? v)
       (ctype-layout-base-type (vector-ref v 0))
@@ -198,8 +199,7 @@
        (if deserialize-inplace
            (cast bs _bytes _ID-pointer)
            (let ([mem (malloc-ID)])
-             (when (copy-any-non-pointers? _ID)
-               (memcpy mem bs 1 _ID))
+             (memcpy mem bs 1 _ID)
              (cast mem _pointer _ID-pointer))))
      (deserialize-cstruct-pointers s ptrs)
      s)
@@ -230,13 +230,8 @@
                (old-fill! (other-vers-unconvert s0))
                (fill! old-v))))))
 
-(define ptr-types '(bytes string/ucs-4 string/utf-16 pointer gcpointer))
+(define ptr-types '(bytes string/ucs-4 string/utf-16 pointer))
 
-(define (copy-any-non-pointers? _ID)
-  (for/or ([t (in-list (ctype->layout _ID))])
-    (let ([base (ctype-layout-base-type t)])
-      (and (not (list? base))
-           (not (memq base ptr-types))))))
 
 (define (serialize-cstruct-pointers o)
   (define who 'serialize-cstruct-pointers)
